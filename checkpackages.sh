af964e0af072569621774bbeac2157caa66b926f
Jan Chaloupka <jchaloup@redhat.com>
2014-10-23 12:15:21 +0200

    Caption for table of repos

--- checkpackages.sh
+++ checkpackages.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "upstream                                 fedora                                   status     repo"
 for repo in $(cat golang.list); do
 	./checkpackage.py $repo
 done
