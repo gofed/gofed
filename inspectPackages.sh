3fc0d195566b6e8f9b22282c3770aea4cb078175
Jan Chaloupka <jchaloup@redhat.com>
2015-01-08 17:22:58 +0100

    Scan all packages automatically

---
+++ inspectPackages.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+script_dir=$(realpath $(dirname $0))
+
+# get packages names
+packages=$(cat $script_dir/golang.packages)
+
+# run scan
+rm -f scan.log
+for pkg_name in $packages; do
+	echo "Scanning ${pkg_name}..."
+	echo "Scanning ${pkg_name}..." >> scan.log
+	$script_dir/inspectpackage.py $pkg_name -o ${pkg_name}.xml >> scan.log
+	echo "" >> scan.log
+	$script_dir/xml2info.py ${pkg_name}.xml
+done
