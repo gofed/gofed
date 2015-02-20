0cc20be4a95e8355b0fb44ba52d9753f0778a801
Jan Chaloupka <jchaloup@redhat.com>
2015-02-20 12:23:19 +0100

    Add detection of strongly connected components to find set of packages with a cyclic buildtime dependency

--- modules/Packages.py
+++ modules/Packages.py
@@ -7,6 +7,8 @@ import os
 import tempfile
 import shutil
 from Repos import detectRepoPrefix
+from ImportPaths import loadImportPathDb
+import operator
 
 GOLANG_PACKAGES="data/golang.packages"
 script_dir = getScriptDir() + "/.."
@@ -172,6 +174,147 @@ def getPackagesFromPkgDb():
 		pkgs.append(line)
 
 	return pkgs
+
+# based on http://www.fit.vutbr.cz/study/courses/GAL/public/gal-slides.pdf
+def transposeGraph(graph):
+	nodes, edges = graph
+	tedges = {}
+	for u in edges:
+		for v in edges[u]:
+			# (u,v) -> (v,u)
+			if v not in tedges:
+				tedges[v] = [u]
+			else:
+				tedges[v].append(u)
+
+	return (nodes, tedges)
+
+class DFS:
+
+	def __init__(self, graph):
+		self.nodes, self.edges = graph
+
+		self.WHITE=0
+		self.GRAY=1
+		self.BLACK=2
+
+		self.color = {}
+		self.d = {}
+		self.f = {}
+		self.time = 0
+		self.pred = {}
+
+
+
+	def DFSVisit(self, node):
+		self.color[node] = self.GRAY
+		self.time += 1
+		self.d[node] = self.time
+
+		if node in self.edges:
+			for adj in self.edges[node]:
+				if self.color[adj] == self.WHITE:
+					self.pred[adj] = node
+					self.DFSVisit(adj)
+
+		self.color[node] = self.BLACK
+		self.time += 1
+		self.f[node] = self.time
+
+	def DFSWalk(self, f = []):
+		for node in self.nodes:
+			self.color[node] = self.WHITE
+			self.pred[node] = ""
+
+		self.time = 0
+
+		if f == []:
+			for node in self.nodes:
+				if self.color[node] == self.WHITE:
+					self.DFSVisit(node)
+			return (self.f, self.d)
+		else:
+			start_nodes = []
+			for node, _ in sorted(f.items(), key=operator.itemgetter(1), reverse=True):
+				if self.color[node] == self.WHITE:
+					self.DFSVisit(node)
+				start_nodes.append(node)
+
+			return (start_nodes, self.pred)
+
+def getSucc(s, pred):
+	if pred[s] == '':
+		return [s]
+	else:
+		return [s] + getSucc(pred[s], pred)
+
+def getSCC(graph):
+	nodes, edges = graph
+	f, d = DFS(graph).DFSWalk()
+	tgraph = transposeGraph(graph)
+	start_nodes, pred = DFS(tgraph).DFSWalk(f)
+	trees = []
+	for node in start_nodes:
+		trees.append(getSucc(node, pred))
+
+	# some trees can overlap
+	scc = []
+	for i_tree in trees:
+		iss = False
+		for j_tree in trees:
+			if i_tree == j_tree:
+				continue
+			if set(i_tree).issubset(j_tree):
+				iss = True
+		if iss == False:
+			scc.append(i_tree)
+
+	return scc
+
+def buildRequirementGraph(verbose=False):
+	# load imported and provided paths
+	ip_provides, ip_imports, pkg_devel_main_pkg = loadImportPathDb()
+	# 1) for each package get a list of all packages it needs
+	# 2) in this list find all cyclic dependencies
+
+	# create mapping of import paths of provides to pkg_devel_name
+	provides_mapping = {}
+	pkgs = []
+	for pkg_devel in ip_provides:
+		pkgs.append(pkg_devel)
+		for ip in ip_provides[pkg_devel]:
+			if ip == "":
+				continue
+
+			provides_mapping[ip] = pkg_devel
+
+
+
+	#print provides_mapping
+	#pkgs = list(set(pkgs))
+	#for line in sorted(pkgs):
+	#	print line
+
+	# Assuming all dependencies will create a sparse matrix, lets use a list of edges instead
+	# (u, v) \in Pkgs \times Pkgs \eq u [B]R v (u needs v)
+	edges = {}
+	for pkg_devel in ip_imports:
+		for ip in ip_imports[pkg_devel]:
+			if ip == "":
+				continue
+
+			if ip not in provides_mapping:
+				if verbose:
+					print "Error: %s path of %s subpackage is not provided by any package" % (ip, pkg_devel)
+				continue
+
+			if pkg_devel not in edges:
+				edges[pkg_devel] = [provides_mapping[ip]]
+			else:
+				if provides_mapping[ip] not in edges[pkg_devel]:
+					edges[pkg_devel].append(provides_mapping[ip])
+
+	return (pkgs, edges)
 
 
 if __name__ == "__main__":
