63f7ae82159427365705bd09b3dbcd4e46a5f0c1
Jan Chaloupka <jchaloup@redhat.com>
2015-02-25 10:50:28 +0100

    API checker: add error handling

--- parseGo.go
+++ parseGo.go
@@ -1,5 +1,125 @@
 package main
 
+/*
+# TODO
+#   - for a given file (*.go source code), list symbols (variables, types,
+#     fields, interfaces, functions, ...), that are used and exported
+#   - do the same for a given directory
+#   - do the same for an entire project (entire directory hiearchy)
+
+###############################################################################
+# Based on a definition of exported identifiers [1]                           #
+#                                                                             #
+# An identifier may be exported to permit access to it from another package.  #
+# An identifier is exported if both:                                          #
+# 1) the first character of the identifier's name is a Unicode upper case     #
+#    letter (Unicode class "Lu"); and                                         #
+# 2) the identifier is declared in the package block or it is a field name or #
+#    method name.                                                             #
+#                                                                             #
+# All other identifiers are not exported.                                     #
+###############################################################################
+# What is identifier? Or what it can be?
+# It can be constant [3], 
+#
+# What is a field name?
+# Struct is composed of fields
+#
+# What is a method name?
+# It is a function with a receiver (function of a struct)
+#
+# Sources:
+# [1]	http://golang.org/ref/spec#Declarations_and_scope
+# [2]	http://golang.org/ref/spec#Struct_types
+# [3]	http://golang.org/ref/spec#Constant_declarations
+# [4]	http://golang.org/ref/spec#Type_declarations
+# [5]	http://golang.org/ref/spec#Function_declarations
+# [6]	http://golang.org/ref/spec#Signature
+# [7]	http://golang.org/pkg/go/ast/#FuncType
+###############################################################################
+# Steps:
+# 1: parse out all definitions of constants (name and value, value to trace
+#    its changes) [3]
+# 2: parse out all definitions of types (name and its value,
+#    can be recursive) [4]
+#
+
+# Function declaration [5]
+# FunctionDecl = "func" FunctionName ( Function | Signature ) .
+# FunctionName = identifier .
+# Function     = Signature FunctionBody .
+# FunctionBody = Block .
+#
+# FunctionBody is not important. Only Signature is [6].
+# Watch out variadic parameter (0-n parametres as ... instead of name)
+#
+# Function signature in a form:
+# "fnc": func_name type type type
+#
+# As variable definition does not have to specify type, it is complicated
+# to determine the type from its value. The value can consist of function
+# calls possibly from different packages. So only its name is extracted.
+#
+###############################################################################
+# struct_json: {'name': "...", 'type': "struct", }
+#
+# id base_type		=>	{'name': id, 'type': base_type}
+# id struct {...}	=>	{'name': id, 'type': 'struct', 'def': ...}
+#
+# GoSymbols = {'consts': [...],'vars': [...],'types': TypesList, 'funcs': [...]
+# TypesList = [{"name": Ident, "type": Type[, "def": TypeDef]},{},...,{}]
+# Type = BaseType | "struct" | ...
+# ### For compounded types
+# TypeDef = 
+#
+###############################################################################
+# PACKAGE NAME AND IMPORT PATH                                                #
+###############################################################################
+# Package names
+# - Go's convention is that the package name is the last element of the import
+#   path: the package imported as "crypto/rot13" should be named rot13. [1]
+#
+# Import paths
+# - An import path (see 'go help packages') denotes a package stored in the
+# local file system. In general, an import path denotes either a standard
+# package (such as "unicode/utf8") or a package found in one of the work spaces
+# (see 'go help gopath'). [2]
+#
+# Relative path
+# An import path beginning with ./ or ../ is called a relative path. [3]
+#
+# Import for side effect
+# - import _ "net/http/pprof"
+# - used only for a side effect
+#
+# If the PackageName is omitted, it defaults to the identifier specified in the
+# package clause of the imported package. [4]
+# If an explicit period (.) appears instead of a name, all the package's
+# exported identifiers declared in that package's package block will be
+# declared in the importing source file's file block and must be accessed
+# without a qualifier. [4]
+# import   "lib/math"    ->     math.Sin
+# import m "lib/math"    ->     m.Sin
+# import . "lib/math"    ->     Sin
+#
+# Another convention is that the package name is the base name of its
+# source directory. [5]
+#
+# <siXy> you can have multiple files in a directory, but they need to be
+# all the same package.
+# <smw> jchaloup, directory and package are 1:1
+# <smw> a package is in only one folder and a folder only contains one package
+#
+# Directory and file names that begin with "." or "_" are ignored by the
+# go tool, as are directories named "testdata".
+#
+# [1] https://golang.org/doc/code.html#PackageNames
+# [2] https://golang.org/cmd/go/#hdr-Import_path_syntax
+# [3] https://golang.org/cmd/go/#hdr-Remote_import_paths
+# [4] http://golang.org/ref/spec#Import_declarations
+# [5] http://golang.org/doc/effective_go.html#names
+*/
+
 import (
 	"fmt"
 	"go/ast"
@@ -7,12 +127,15 @@ import (
 	"go/token"
 	"os"
 	"strings"
+	"path"
 )
 
+// array2JSON transforms an array of strings into JSON.
 func array2JSON(arr []string) (json string) {
 	return "[" + strings.Join(arr, ", ") + "]"
 }
 
+// map2JSON transforms a map of strings into JSON.
 func map2JSON(def map[string]string) (json string) {
 	arr := make([]string, 0)
 	for key, value := range def {
@@ -25,11 +148,17 @@ func map2JSON(def map[string]string) (json string) {
 	return
 }
 
-func parseStruct(t *ast.StructType) (types []string) {
+// parseStruct returns all fields of struct.
+// It returns an array of fields.
+func parseStruct(t *ast.StructType) (types []string, err int) {
 	if t.Fields.List != nil {
+		var sig string
 		for _, f := range t.Fields.List {
 			// get type signature
-			sig := parseTypes(f.Type, "")
+			sig, err = parseTypes(f.Type, "")
+			if err != 0 {
+				return
+			}
 			if sig == "" {
 				continue
 			}
@@ -49,30 +178,52 @@ func parseStruct(t *ast.StructType) (types []string) {
 	return
 }
 
-func parseFunc(decl *ast.FuncDecl) (sig string) {
-	recv := getSymbolReceiver(decl.Recv)
-	params := getSymbolParams(decl.Type.Params)
-	results := getSymbolResults(decl.Type.Results)
+// parseFunc transforms a function declaration into a JSON.
+func parseFunc(decl *ast.FuncDecl) (sig string, err int) {
+	var recv, params, results []string
+
+	recv, err = getSymbolReceiver(decl.Recv)
+	if err != 0 {
+		return
+	}
+
+	params, err = getSymbolParams(decl.Type.Params)
+	if err != 0 {
+		return
+	}
+
+	results, err = getSymbolResults(decl.Type.Results)
+	if err != 0 {
+		return
+	}
+
 	sig = fncSignature2JSON(decl.Name.Name, recv, params, results)
 	return
 }
 
-// Returns JSON definition of a type (possibly recursive)
-func parseTypes(et ast.Expr, name string) (sig string) {
-	//fmt.Println(et)
+// parseTypes returns JSON definition of a type (possibly recursive).
+func parseTypes(et ast.Expr, name string) (sig string, err int) {
+	err = 0
+	var d string
+	var s []string
 	switch t := et.(type) {
 	case *ast.StarExpr:
-		//fmt.Print("StarExpr:")
 		sig = "{'type': 'pointer', 'def': "
-		sig += parseTypes(t.X, "")
+		d, err = parseTypes(t.X, "")
+		if err != 0 {
+			return
+		}
+		sig += d
 		sig += "}"
 	case *ast.Ident:
-		//fmt.Print("Ident:")
 		sig = "{'type': " + t.Name + "}"
 	case *ast.SelectorExpr:
-		//fmt.Print("Selector:")
 		sig = "{'type': 'selector', 'prefix': "
-		sig += parseTypes(t.X, "") + ", 'item': " + t.Sel.Name + "}"
+		d, err = parseTypes(t.X, "")
+		if err != 0 {
+			return
+		}
+		sig += d + ", 'item': " + t.Sel.Name + "}"
 	case *ast.ChanType:
 		// {'type': 'channel', 'dir': ..., 'value': ...}
 		tp := make(map[string]string)
@@ -85,23 +236,38 @@ func parseTypes(et ast.Expr, name string) (sig string) {
 		default:
 			tp["dir"] = "3"
 		}
-		tp["value"] = parseTypes(t.Value, "")
+		d, err = parseTypes(t.Value, "")
+		if err != 0 {
+			return
+		}
+		tp["value"] = d
 		sig = map2JSON(tp)
 	case *ast.StructType:
-		//fmt.Println("Struct:")
 		// {'name': id, 'type': 'struct', 'def': ...}
 		tp := make(map[string]string)
 		tp["name"] = name
 		tp["type"] = "struct"
-		tp["def"]  = array2JSON(parseStruct(t))
+		s, err = parseStruct(t)
+		if err != 0 {
+			return
+		}
+		tp["def"] = array2JSON(s)
 		sig = map2JSON(tp)
 	case *ast.MapType:
 		// {'name': id, 'type': 'map', 'keytype': ..., 'valuetype': ...}
 		tp := make(map[string]string)
 		tp["name"] = name
 		tp["type"] = "map"
-		tp["keytype"] = parseTypes(t.Key, "")
-		tp["valuetype"] = parseTypes(t.Value, "")
+		d, err = parseTypes(t.Key, "")
+		if err != 0 {
+			return
+		}
+		tp["keytype"] = d
+		d, err = parseTypes(t.Value, "")
+		if err != 0 {
+			return
+		}
+		tp["valuetype"] = d
 		sig = map2JSON(tp)
 	case *ast.ArrayType:
 		tp := make(map[string]string)
@@ -111,15 +277,27 @@ func parseTypes(et ast.Expr, name string) (sig string) {
 		} else {
 			tp["type"] = "array"
 			// http://golang.org/ref/spec#ArrayType
-			// it must evaluate to a non-negative constant representable by a value of type int
+			// it must evaluate to a non-negative constant
+			// representable by a value of type int
 			//fmt.Println(getArrayLen(t.Len))
 			//tp["len"]  = parseTypes(t.Len, "")
 		}
-		tp["elmtype"] = parseTypes(t.Elt, "")
+		d, err = parseTypes(t.Elt, "")
+		if err != 0 {
+			return
+		}
+		tp["elmtype"] = d
 		sig = map2JSON(tp)
 	case *ast.FuncType:
-		params := getSymbolParams(t.Params)
-		results := getSymbolResults(t.Results)
+		var params, results []string
+		params, err = getSymbolParams(t.Params)
+		if err != 0 {
+			return
+		}
+		results, err = getSymbolResults(t.Results)
+		if err != 0 {
+			return
+		}
 		tp := make(map[string]string)
 		tp["type"] = "func"
 		tp["params"] = "[" + strings.Join(params, ", ") + "]"
@@ -136,19 +314,23 @@ func parseTypes(et ast.Expr, name string) (sig string) {
 		fmt.Println("Ellipsis")
 	*/
 	default:
+		err = 1
 		fmt.Println("Error: check for symbol not implemented.")
 		fmt.Println(et)
 		fmt.Println(t)
-		return ""
 	}
 	return
 }
 
-
-func getSymbolReceiver(fl *ast.FieldList) (types []string) {
+// getSymbolReceiver returns receiver of a function as a list of types.
+func getSymbolReceiver(fl *ast.FieldList) (types []string, err int) {
 	if fl != nil {
+		var sig string
 		for _, field := range (*fl).List {
-			sig := parseTypes(field.Type, "")
+			sig, err = parseTypes(field.Type, "")
+			if err != 0 {
+				return
+			}
 			for i := 0; i < len(field.Names); i++ {
 				// parameter type
 				types = append(types, sig)
@@ -158,9 +340,14 @@ func getSymbolReceiver(fl *ast.FieldList) (types []string) {
 	return
 }
 
-func getSymbolParams(fl *ast.FieldList) (types []string) {
+// getSymbolParams returns params of a function as a list of types.
+func getSymbolParams(fl *ast.FieldList) (types []string, err int) {
+	var sig string
 	for _, field := range (*fl).List {
-		sig := parseTypes(field.Type, "")
+		sig, err = parseTypes(field.Type, "")
+		if err != 0 {
+			return
+		}
 		for i := 0; i < len(field.Names); i++ {
 			types = append(types, sig)
 		}
@@ -168,10 +355,15 @@ func getSymbolParams(fl *ast.FieldList) (types []string) {
 	return
 }
 
-func getSymbolResults(fl *ast.FieldList) (types []string) {
+// getSymbolResults returns results type of a function as a list of types.
+func getSymbolResults(fl *ast.FieldList) (types []string, err int) {
 	if fl != nil {
+		var sig string
 		for _, field := range fl.List {
-			sig := parseTypes(field.Type, "")
+			sig, err = parseTypes(field.Type, "")
+			if err != 0 {
+				return
+			}
 			rt := len(field.Names)
 			for i := 0; i < rt; i++ {
 				types = append(types, sig)
@@ -184,7 +376,9 @@ func getSymbolResults(fl *ast.FieldList) (types []string) {
 	return
 }
 
-func fncSignature2JSON(name string, recv []string, params []string, returns []string) (json string) {
+// fncSignature2JSON return function signature as a JSON.
+func fncSignature2JSON(name string, recv []string, params []string,
+     returns []string) (json string) {
 	json = name + ": {"
 	json += "recv: [" + strings.Join(recv, ", ") + "], "
 	json += "params: [" + strings.Join(params, ", ") + "], "
@@ -193,6 +387,67 @@ func fncSignature2JSON(name string, recv []string, params []string, returns []st
 	return
 }
 
+type Symbols struct {
+	typeDefs []string
+	funcDefs []string
+	varcons  []string
+	imports  []string
+}
+
+func (symbols * Symbols) ToJSON() (json string) {
+	dict := make(map[string]string)
+	dict["types"] = array2JSON(symbols.typeDefs)
+	dict["funcs"] = array2JSON(symbols.funcDefs)
+	dict["vars"]  = array2JSON(symbols.varcons)
+	dict["imports"] = array2JSON(symbols.imports)
+	return map2JSON(dict)
+}
+
+func (symbols * Symbols) AddImport(d *ast.ImportSpec) {
+	sig := "{'name': '"
+	if d.Name != nil {
+		sig += d.Name.Name
+	// If the PackageName is omitted, it defaults to the identifier
+	// specified in the package clause of the imported package, .i.e.
+	// use a basename of the path
+	} else if (d.Path.Value != "") {
+		sig += path.Base(d.Path.Value)
+	}
+	sig += "', 'path': '" + d.Path.Value + "'"
+	symbols.imports = append(symbols.imports, sig)
+}
+
+func (symbols * Symbols) AddVar(d *ast.ValueSpec) {
+	for _, name := range d.Names {
+		if ast.IsExported(name.Name) {
+			symbols.varcons = append(symbols.varcons, name.Name)
+		}
+	}
+}
+
+func (symbols * Symbols) AddTypes(d *ast.TypeSpec) (err int) {
+	// {'name': id, 'type': 'struct', 'def': ...}
+	var def string
+	def, err = parseTypes(d.Type, d.Name.Name)
+	if err != 0 {
+		return
+	}
+	symbols.typeDefs = append(symbols.typeDefs, def)
+	return
+}
+
+func (symbols * Symbols) AddFunc(decl *ast.FuncDecl) (err int) {
+	if ast.IsExported(decl.Name.Name) {
+		var json string
+		json, err = parseFunc(decl)
+		if err != 0 {
+			return
+		}
+
+		symbols.funcDefs = append(symbols.funcDefs, json)
+	}
+	return
+}
 
 func main() {
 	fset := token.NewFileSet() // positions are relative to fset
@@ -210,10 +465,7 @@ func main() {
 		return
 	}
 
-	typeDefs := make([]string, 0)
-	funcDefs := make([]string, 0)
-	varcons  := make([]string, 0)
-	imports  := make([]string, 0)
+	symbols := new(Symbols)
 
 	// Print the imports from the file's AST.
 	for _, d := range f.Decls {
@@ -225,50 +477,20 @@ func main() {
 				//fmt.Println(spec)
 				switch d := spec.(type) {
 				case *ast.ImportSpec:
-					sig := "{'name': '"
-					if d.Name != nil {
-						sig += d.Name.Name
-					}
-					// if d.Name is '', use a basename of the path?
-					sig += "', 'path': '" + d.Path.Value + "'"
-					imports = append(imports, sig)
+					symbols.AddImport(d)
 				case *ast.ValueSpec:
-					for _, name := range d.Names {
-						if ast.IsExported(name.Name) {
-							varcons = append(varcons, name.Name)
-						}
-					}
+					symbols.AddVar(d)
 				case *ast.TypeSpec:
-					// {'name': id, 'type': 'struct', 'def': ...}
-					def := parseTypes(d.Type, d.Name.Name)
-					typeDefs = append(typeDefs, def)
+					err := symbols.AddTypes(d)
+					if err != 0 {
+						os.Exit(err)
+					}
 				}
 			}
 		case *ast.FuncDecl:
-			if ast.IsExported(decl.Name.Name) {
-				json := parseFunc(decl)
-				funcDefs = append(funcDefs, json)
-			}
+			symbols.AddFunc(decl)
 		}
 	}
-	//return
-	fmt.Println("")
-	fmt.Println("TYPE DEFS:")
-	for _, item := range typeDefs {
-		fmt.Println(item)
-	}
-	fmt.Println("")
-	fmt.Println("FUNC DEFS:")
-	for _, item := range funcDefs {
-		fmt.Println(item)
-	}
-	fmt.Println("")
-	fmt.Println("VAR DEFS:")
-	fmt.Println(varcons)
-	fmt.Println("")
-	fmt.Println("IMPORTS DEFS:")
-	for _, item := range imports {
-		fmt.Println(item)
-	}
-
+	fmt.Println(symbols.ToJSON())
+	os.Exit(0)
 }
