f5c07ed000f5ad8ef4aa1e0e03c9d13269301d4b
Jan Chaloupka <jchaloup@redhat.com>
2015-03-11 16:10:33 +0100

    API checker: move comparing classes to GoSymbols module, add +/- signs before some error messages

--- modules/GoSymbols.py
+++ modules/GoSymbols.py
@@ -288,7 +288,7 @@ class PackageToXml:
 			node.append(elt_node)
 
 		elif type_name == TYPE_CHANNEL:
-			node.set("type", "chan")
+			node.set("type", "channel")
 			if "dir" not in type_def:
 				node.set("dir", type_def["def"]["dir"])
 				err, val_node = self.typeToXML(type_def["def"]["value"])
@@ -609,4 +609,647 @@ class ProjectToXml:
 
 	def getError(self):
 		return self.err
+
+
+class CompareTypes:
+
+	def __init__(self, debug=False):
+		self.debug = debug
+
+	def compareIdents(self, ident1, ident2):
+		"""
+		Identifier is defined by type (def attribute) and name (name attribute).
+		Name attribute is presented only for the top most definitions.
+
+		Both identifiers are identical if their names (if presented) and defs are the same.
+
+		ident1, ident2: etree.Element nodes
+		"""
+		#<field type="ident" def="string"[ name="Action"]/>
+		err = []
+
+		if ident1.get("def") != ident2.get("def"):
+			err.append("-identifiers differ in type: %s != %s" % (ident1.get("def"), ident2.get("def")))
+
+		if "name" in ident1.keys() and "name" in ident2.keys():
+			if ident1.get("name") != ident2.get("name"):
+				err.append("-identifiers differ in name: %s != %s" % (ident1.get("name"), ident2.get("name")))
+
+			if self.debug:
+				print "#NAME: " + ident1.get("name")
+
+		return err
+
+	def comparePointers(self, pointer1, pointer2):
+		"""
+		Pointer is defined by a type it points to.
+		Type is a child element.
+
+		pointer1, pointer2: etree.Element nodes
+		"""
+		# <field type="pointer" name="Node">
+		return self.compareTypes(pointer1[0], pointer2[0])
+
+	def compareSelectors(self, selector1, selector2):
+		"""
+		Selector is defined by prefix and item.
+
+		expression.selector
+
+		Two selectors are identical if they have the same
+		expression (prefix tag) and selector (item tag).
+
+		Expression is always identifier.
+		"""
+		err = []
+
+		prefix1, prefix2 = None, None
+		item1, item2 = None, None
+
+		for node in selector1:
+			if node.tag == "prefix":
+				prefix1 = node
+			elif node.tag == "item":
+				item1 = node
+
+		for node in selector2:
+			if node.tag == "prefix":
+				prefix2 = node
+			elif node.tag == "item":
+				item2 = node
+
+		if prefix1.get("value") != prefix2.get("value"):
+			err.append("-Selectors differ in selector: %s != %s" %
+			(prefix1.get("value"), prefix2.get("value")))
+
+		if item1.get("value") != item2.get("value"):
+			err.append("-Selectors differ in expression: %s != %s" %
+			(item1.get("value"), item2.get("value")))
+
+		return err
+
+	def compareChannels(self, chan1, chan2):
+		"""
+		Channel is defined by a direction (dir attribute)
+		and type (type tag).
+
+		Channels are identical if their direction and type
+		are identical.
+
+		chan1, chan2: etree.Element nodes
+		"""
+		#<field type="chan" dir="3" name="stop">
+		#  <type type="struct" name=""/>
+		#</field>
+		err = []
+
+		if chan1.get("dir") != chan2.get("dir"):
+			err.append("-Channels has different direction:"
+			  " %s != %s" % (chan1.get("dir"), chan2.get("dir")))
+
+		e = self.compareTypes(chan1[0], chan2[0])
+		if e != []:
+			err += e
+
+		return err	
+
+	def compareEllipsises(self, ellipsis1, ellipsis2):
+
+		"""
+		Ellipsis is defined by a type (type tag).
+
+		Two ellipsises are identical if their types are identical.
+
+		ellipsis1, ellipsis2: etree.Element nodes
+		"""
+		#<type type="ellipsis">
+		#  <type type="ident" def="string"/>
+		#</type>
+		return self.compareTypes(ellipsis1[0], ellipsis2[0])
+
+	def compareSlices(self, slice1, slice2):
+		"""
+		Slice is defined by element type (elmtype tag).
+
+		Slices are identical if element types are identical.
+		"""
+		# <elmtype type="ident" def="string"/>
+		return self.compareTypes(slice1[0], slice2[0])
+
+	def compareMaps(self, map1, map2):
+
+		"""
+		Map is defined by a keytype (keytype tag) and
+		valuetype (valuetype tag).
+
+		Two maps are identical if keytypes and valuetypes
+		are identical.
+
+		map1, map2: etree.Element nodes
+		"""
+		err = []
+
+		value1, value2 = None, None
+		key1, key2 = None, None
+
+		for node in map1:
+			if node.tag == "keytype":
+				key1 = node
+			elif node.tag == "valtype":
+				value1 = node
+
+		for node in map2:
+			if node.tag == "keytype":
+				key2 = node
+			elif node.tag == "valtype":
+				value2 = node
+
+		e = self.compareTypes(key1, key2)
+		if e != []:
+			err += e
+
+		e = self.compareTypes(value1, value2)
+		if e != []:
+			err += e
+
+		return err
+
+	def compareFunctions(self, function1, function2):
+
+		"""
+		Function is defined by signature.
+		Signature consists of:
+		  methods' name (name attribute)
+		  list of parameters (<paramsList>)
+		  list of results (<resultsList>)
+
+		Two functions are identical if their signatures are identical.
+
+		function1, function2: etree.Element nodes
+
+		"""
+		#<type name="InterruptHandler" type="func">
+		#   <paramsList/>
+		#   <resultsList/>
+		#</type>
+		err = []
+
+		# check names
+		if function1.get("name") != function2.get("name"):
+			err.append("-function's name changed: %s -> %s" %
+				(function1.get("name"), function2.get("name")))
+			return err
+
+		params1 = None
+		params2 = None
+		results1 = None
+		results2 = None
+
+		for node in function1:
+			if node.tag == "paramsList":
+				params1 = node
+			elif node.tag == "resultsList":
+				results1 = node
+		for node in function2:
+			if node.tag == "paramsList":
+				params2 = node
+			elif node.tag == "resultsList":
+				results2 = node
+
+		# check parameters
+		l1 = len(params1)
+		l2 = len(params2)
+		if l1 != l2:
+			err.append("-function %s: parameter count changed: "
+				"%s -> %s" % (function1.get("name"), l1, l2))
+			return err
+
+		for i in range(0, len(params1)):
+			e = self.compareTypes(params1[i], params2[i])
+			if e != []:
+				err += e
+
+		# check results
+		l1 = len(results1)
+		l2 = len(results2)
+		if l1 != l2:
+			err.append("-function %s: result count changed: "
+				"%s -> %s" % (function1.get("name"), l1, l2))
+			return err
+
+		for i in range(0, len(results1)):
+			e = self.compareTypes(results1[i], results2[i])
+			if e != []:
+				err += e
+
+		return err
+
+	def compareMethods(self, method1, method2):
+		"""
+		Method is defined by signature.
+		Signature consists of:
+		  methods' name (name attribute)
+		  reciever
+		  list of parameters (<paramsList>)
+		  list of results (<resultsList>)
+
+		Two methods are identical if their signatures are identical.
+
+		method1, method2: etree.Element nodes
+		"""
+
+		err = []
+
+		# check names
+		if method1.get("name") != method2.get("name"):
+			err.append("-method's name differs: %s != %s" %
+				(method1.get("name"), method2.get("name")))
+			return err
+
+		params1 = None
+		params2 = None
+		results1 = None
+		results2 = None
+
+		for node in method1:
+			if node.tag == "paramsList":
+				params1 = node
+			elif node.tag == "resultsList":
+				results1 = node
+			else:
+				print "method receiver tag????"
+
+		for node in method2:
+			if node.tag == "paramsList":
+				params2 = node
+			elif node.tag == "resultsList":
+				results2 = node
+			else:
+				print "method receiver tag????"
+
+		# check parameters
+		if len(params1) != len(params2):
+			err.append("-methods differs in parameter count")
+			return err
+
+		for i in range(0, len(params1)):
+			e = self.compareTypes(params1[i], params2[i])
+			if e != []:
+				err += e
+
+		# check results
+		if len(results1) != len(results2):
+			err.append("-methods differs in result count")
+			return err
+
+		for i in range(0, len(results1)):
+			e = self.compareTypes(results1[i], results2[i])
+			if e != []:
+				err += e
+
+		# check receivers?
+		return err
+
+	def compareInterfaces(self, interface1, interface2):
+
+		err = []
+		if self.debug:
+			print "#INTERFACE: %s" % interface1.get("name")
+
+		m1_set = set(map(lambda m: m.get("name"), interface1[:]))
+		m2_set = set(map(lambda m: m.get("name"), interface2[:]))
+
+		new_ms = list(m2_set - m1_set)
+		rem_ms = list(m1_set - m2_set)
+		com_ms = list(m1_set & m2_set)
+
+		if new_ms != []:
+			print "+new methods: " + ", ".join(new_ms)
+
+		if rem_ms != []:
+			err.append("-interface %s removed methods: %s" %
+				(interface1.get("name"), ", ".join(rem_ms)))
+
+		m1s_dir = {}
+		m2s_dir = {}
+		for method in interface1:
+			name = method.get("name")
+			if name in com_ms:
+				m1s_dir[name] = method
+
+		for method in interface2:
+			name = method.get("name")
+			if name in com_ms:
+				m2s_dir[name] = method
+
+		for method_name in com_ms:
+			e = self.compareMethods(m1s_dir[method_name],
+				m2s_dir[method_name])
+			if e != []:
+				err += e
+
+			if self.debug:
+				print "#METHOD: %s" % method_name
+
+		return err
+
+	def compareTypes(self, type1, type2):
+
+		err = []
+		type = type1.get("type")
+		if type != type2.get("type"):
+			err.append("-type differs")
+			return err
+
+		#print type
+		if type == TYPE_INTERFACE:
+			return self.compareInterfaces(type1, type2)
+		elif type == TYPE_IDENT:
+			return self.compareIdents(type1, type2)
+		elif type == TYPE_POINTER:
+			return self.comparePointers(type1, type2)
+		elif type == TYPE_SELECTOR:
+			return self.compareSelectors(type1, type2)
+		elif type == TYPE_CHANNEL:
+			return self.compareChannels(type1, type2)
+		elif type == TYPE_ELLIPSIS:
+			return self.compareEllipsises(type1, type2)
+		elif type == TYPE_SLICE:
+			return self.compareSlices(type1, type2)
+		elif type == TYPE_MAP:
+			return self.compareMaps(type1, type2)
+		elif type == TYPE_STRUCT:
+			return self.compareStructs(type1, type2)
+		elif type == TYPE_FUNC:
+			return self.compareFunctions(type1, type2)
+		else:
+			#TYPE_ARRAY = "array"
+			#TYPE_METHOD = "method"
+			#TYPE_PARENTHESIS = "parenthesis"
+			print "%s type not implemented yet" % type
+			exit(0)
+			return []
+
+		return []
+
+	def constructTypeQualifiedName(self, type, full=False):
+		"""
+		For given type construct its full qualified name.
+
+		AnonymousField = [ "*" ] TypeName .
+		TypeName  = identifier | QualifiedIdent .
+		QualifiedIdent = PackageName "." identifier .
+		"""
+		t = type.get("type")
+		if t == "ident":
+			if self.debug:
+				print "#FQN: %s" % type.get("def")
+			return type.get("def")
+		elif t == "pointer":
+			return self.constructTypeQualifiedName(type[0])
+		elif t == "selector":
+			expr, sel = None, None
+			for node in type:
+				if node.tag == "prefix":
+					expr = node.get("value")
+				elif node.tag == "item":
+					sel = node.get("value")
+			if full:
+				return "%s.%s" % (expr, sel)
+			else:
+				return sel
+		else:
+			print "Type %s can not be used for FQN" % t
+			return ""
+
+	def compareStructs(self, struct1, struct2):
+		"""
+		Struct is defined by its fields.
+		Field consists of a name (name attribute) and type definition.
+
+		Order of fields is important but can be different.
+
+		For anonymous field an unqualified type name is taken.
+
+		Structs are identical if they have the same number of fields
+		and list of fiels are identical.
+
+		struct1, struct2: etree.Element nodes
+		"""
+		err = []
+
+		if self.debug:
+			print "#STRUCT: %s" % struct1.get("name")
+
+		# get a list of field names (with anynomous as well)
+		fs1 = map(lambda f: f.get("name") if f.get("name") != ""
+			else self.constructTypeQualifiedName(f), struct1)
+		fs2 = map(lambda f: f.get("name") if f.get("name") != ""
+			else self.constructTypeQualifiedName(f), struct2)
+
+		index1, index2 = 0, 0
+		l1, l2 = len(struct1), len(struct2)
+
+		if l1 != l2:
+			err.append("struct %s has different number of"
+				" fields" % struct1.get("name"))
+
+		while index1 < l1 and index2 < l2:
+
+			name1 = fs1[index1]
+			name2 = fs2[index2]
+
+			if name1 != name2:
+				err.append("struct %s: fields are reordered" %
+					struct1.get("name"))
+				break
+
+			index1 += 1
+			index2 += 1
+
+		s1_dict, s2_dict = {}, {}
+		for node in struct1:
+			name = node.get("name")
+			if name == "":
+				name = self.constructTypeQualifiedName(node)
+
+			s1_dict[name] = node
+
+		for node in struct2:
+			name = node.get("name")
+			if name == "":
+				name = self.constructTypeQualifiedName(node)
+
+			s2_dict[name] = node
+
+		# check individual fields
+		fs1 = sorted(fs1)
+		fs2 = sorted(fs2)
+		while index1 < l1 and index2 < l2:
+			# check types
+			name1 = fs1[index1]
+			name2 = fs2[index2]
+			if name1 == name2:
+				index1 += 1
+				index2 += 1
+				e = self.compareTypes(s1_dict[name1],
+					s2_dict[name2])
+				if e != []:
+					err += e
+			elif name1 < name2:
+				index1 += 1
+				err.append("-struct %s: %s field removed" %
+					(struct1.get("name"), name1))
+			else:
+				index2 += 1
+				err.append("+struct %s: new field '%s'" %
+					(struct1.get("name"), name2))
+
+		# some fields not checked?
+		while index1 < l1:
+			err.append("-struct %s: field '%s' removed" %
+				(struct1.get("name"), fs1[index1]))
+			index1 += 1
+
+		while index2 < l2:
+			err.append("+struct %s: new field '%s'" %
+				(struct1.get("name"), fs2[index2]))
+			index2 += 1
+
+		return err
+
+class ComparePackages:
+
+	def __init__(self, pkg_name):
+		self.pkg_name = pkg_name
+
+	def compareNames(self, names1, names2):
+		msg = []
+
+		names1_set = set(map(lambda i: i.get('value'), names1))
+		names2_set = set(map(lambda i: i.get('value'), names2))
+
+		new_names = list(names2_set - names1_set)
+		rem_names = list(names1_set - names2_set)
+
+		if new_names != []:
+			msg.append("+new variables/constants: " + ", ".join(new_names))
+
+		if rem_names != []:
+			msg.append("-removed variables/constants: " + ", ".join(rem_names))
+
+		return msg
+
+	def compareFunctions(self, funcs1, funcs2):
+		msg = []
+
+		# get types names
+		funcs1_set = set(map(lambda x: x.get("name"), funcs1[:]))
+		funcs2_set = set(map(lambda x: x.get("name"), funcs2[:]))
+
+		new_funcs = list(funcs2_set - funcs1_set)
+		rem_funcs = list(funcs1_set - funcs2_set)
+		com_funcs = list(funcs1_set & funcs2_set)
+
+		if new_funcs != []:
+			msg.append("+new functions: " + ", ".join(new_funcs))
+
+		if rem_funcs != []:
+			msg.append("-removed functions: " + ", ".join(rem_funcs))
+
+		fs1_dict, fs2_dict = {}, {}
+		for node in funcs1:
+			node.set("type", "func")
+			fs1_dict[node.get("name")] = node
+
+		for node in funcs2:
+			node.set("type", "func")
+			fs2_dict[node.get("name")] = node
+
+		for name in com_funcs:
+			e = CompareTypes().compareTypes(fs1_dict[name], fs2_dict[name])
+			if e != []:
+				msg += e
+
+		return msg
+
+	def compareTypes(self, type1, type2):
+		msg = []
+
+		# get types names
+		type1_set = set(map(lambda x: x.get("name"), type1[:]))
+		type2_set = set(map(lambda x: x.get("name"), type2[:]))
+
+		new_types = list(type2_set - type1_set)
+		rem_types = list(type1_set - type2_set)
+		com_types = list(type1_set & type2_set)
+
+		if new_types != []:
+			msg.append("+new types: " + ", ".join(new_types))
+
+		if rem_types != []:
+			msg.append("-removed types: " + ", ".join(rem_types))
+
+		types1_dir = {}
+		types2_dir = {}
+		for type in type1:
+			if type.get("name") in com_types:
+				types1_dir[type.get("name")] = type
+
+		for type in type2:
+			if type.get("name") in com_types:
+				types2_dir[type.get("name")] = type
+
+		for type in com_types:
+			#print types1_dir[type].get("name")
+			err = CompareTypes().compareTypes(types1_dir[type], types2_dir[type])
+			if err != []:
+				msg += err
+
+		return msg
+
+	def comparePackages(self, pkg1, pkg2):
+		"""
+		Top most definitions can contain method definition.
+		However as the method is field of a struct, it is already
+		checked during checking of all types.
+		"""
+		msg = []
+
+		types1 = None
+		funcs1 = None
+		names1 = None
+
+		types2 = None
+		funcs2 = None
+		names2 = None
+
+		for elm in pkg1:
+			if elm.tag == "types":
+				types1 = elm
+			elif elm.tag == "functions":
+				funcs1 = elm
+			elif elm.tag == "names":
+				names1 = elm
+
+		for elm in pkg2:
+			if elm.tag == "types":
+				types2 = elm
+			elif elm.tag == "functions":
+				funcs2 = elm
+			elif elm.tag == "names":
+				names2 = elm
+
+		# check names
+		msg += self.compareNames(names1, names2)
+
+		# check types
+		msg += self.compareTypes(types1, types2)
+
+		# check funcs
+		msg += self.compareFunctions(funcs1, funcs2)
+
+		if msg != []:
+			print "Package: %s" % self.pkg_name
+			print "\n".join(map(lambda m: "\t" + m, msg))
 
