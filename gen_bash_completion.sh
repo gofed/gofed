116bb717ff8ca6d69a6d004f0526f8cd05093979
Jan Chaloupka <jchaloup@redhat.com>
2015-04-01 14:41:41 +0200

    bash_completion: add options to pull push scratch-build bbo

--- gen_bash_completion.sh
+++ gen_bash_completion.sh
@@ -2,7 +2,7 @@
 # 1 - package name
 pkg=$1
 
-HELP="apidiff build check-commit check-deps gcpmaster ggi inspect lint scandeps scan-imports scaninfo scansymbols scan-packages wizard tools"
+HELP="apidiff build check-commit check-deps gcpmaster ggi inspect lint scandeps scan-imports scaninfo scansymbols scan-packages wizard tools pull push scratch-build bbo"
 rm -f tools.options
 for cmd in $HELP; do
 	python parseOptions.py $cmd >> tools.options
