a64a617dfd698ba9f580011606d510f044bccad4
Jan Chaloupka <jchaloup@redhat.com>
2015-04-19 01:48:46 +0200

    Add Logger and FormatedPrint

--- modules/Utils.py
+++ modules/Utils.py
@@ -1,6 +1,7 @@
 from subprocess import PIPE
 from subprocess import Popen
 import os
+import sys
 
 RED = '\033[91m'
 GREEN = '\033[92m'
@@ -14,6 +15,64 @@ BLACK = '\033[90m'
 DEFAULT = '\033[99m'
 ENDC = '\033[0m'
 
+def getScriptDir():
+	return os.path.dirname(os.path.realpath(__file__))
+
+LOG_LEVEL_ERROR = 0
+LOG_LEVEL_WARNING = 1
+LOG_LEVEL_INFO = 2
+
+LOG_FILE = "/var/lib/gofed/gofed.log"
+
+class Logger:
+
+	def __init__(self, level=LOG_LEVEL_INFO):
+		self.level = level
+		#self.log_file = open(LOG_FILE, "w")
+		self.log_file = sys.stderr
+
+	#def __del__(self):
+	#	self.log_file.close()	
+
+	def log(self, msg, level=""):
+		print msg
+		print LOG_FILE
+
+		if level == "":
+			self.log_file.write("%s: %s\n" % (self.level, msg))
+		else:
+			self.log_file.write("%s: %s\n" % (level, msg))
+		
+class FormatedPrint:
+
+	def __init__(self, formated=True):
+		self.formated = formated
+
+	def printError(self, msg):
+		if self.formated:
+			print "%sError: %s%s" % (RED, msg, ENDC)
+		else:
+			print "Error: %s" % msg
+
+	def printWarning(self, msg):
+		if self.formated:
+			print "%sWarning: %s%s" % (YELLOW, msg, ENDC)
+		else:
+			print "Warning: %s" % msg
+
+	def printInfo(self, msg):
+		if self.formated:
+			print "%s%s%s" % (BLUE, msg, ENDC)
+		else:
+			print "%s" % msg
+
+	def printProgress(self, msg):
+		if self.formated:
+			print "%s%s%s" % (YELLOW, msg, ENDC)
+		else:
+			print "%s" % msg
+
+
 def runCommand(cmd):
 	#cmd = cmd.split(' ')
 	process = Popen(cmd, stderr=PIPE, stdout=PIPE, shell=True)
@@ -22,8 +81,6 @@ def runCommand(cmd):
 
 	return stdout, stderr, rt
 
-def getScriptDir():
-	return os.path.dirname(os.path.realpath(__file__))
 
 def inverseMap(mfnc):
 	"""inverse mapping of multifunction
