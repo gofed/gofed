1ca4942188fb4b20bc6d28136269f95e61ebd921
Jan Chaloupka <jchaloup@redhat.com>
2015-11-06 00:17:01 +0100

    approxDepsFile.py: add options and some user interface

--- approxDepsFile.py
+++ approxDepsFile.py
@@ -3,8 +3,11 @@ from modules.Config import Config
 
 import json
 import datetime
+import sys
 
 from modules.ParserConfig import ParserConfig
+
+import optparse
 
 # get imported packages from GoSymbolsExtractor
 # decompose packages into classes
@@ -13,27 +16,84 @@ from modules.ParserConfig import ParserConfig
 
 # optionally get a list of imported packages for given subset of project's packages
 
-def detectProjectSubpackages(prefix, imported_packages):
-	subpackages = []
-	prefix_len = len(prefix)
-	for ip in imported_packages:
-		if ip.startswith(prefix):
-			subpackage = ip[prefix_len:]
-			if subpackage == "":
-				subpackage = "."
-			else:
-				subpackage = subpackage[1:]
-			subpackages.append( subpackage )
+def deps2GodepsJson(deps, import_path):
+	json_content = {}
+	json_content["ImportPath"] = import_path
 
-	return subpackages
+	json_deps = []
+	for ip in deps:
+		json_dep = {}
+		json_dep["ImportPath"] = deps[ip]["ImportPath"]
+		json_dep["Comment"] = deps[ip]["Date"]
+		json_dep["Rev"] = deps[ip]["Rev"]
+		json_deps.append(json_dep)
 
+	json_content["Deps"] = json_deps
+	return json.dumps(json_content, indent=4, sort_keys=False)
 
 if __name__ == "__main__":
 
+	parser = optparse.OptionParser("%prog [-a] [-c] [-d [-v]] [directory]")
+
+	parser.add_option_group( optparse.OptionGroup(parser, "directory", "Directory to inspect. If empty, current directory is used.") )
+
+	parser.add_option(
+	    "", "", "--json", dest="json", action = "store_true", default = False,
+	    help = "Display dependencies as json (almost Godeps.json)"
+	)
+
+	parser.add_option(
+            "", "", "--scan-all-dirs", dest="scanalldirs", action = "store_true", default = False,
+            help = "Scan all dirs, including Godeps directory"
+        )
+
+	parser.add_option(
+            "", "-v", "--verbose", dest="verbose", action = "store_true", default = False,
+            help = "Verbose mode"
+        )
+
+	parser.add_option(
+            "", "", "--skip-dirs", dest="skipdirs", default = "",
+            help = "Scan all dirs except specified via SKIPDIRS. Directories are comma separated list."
+        )
+
+	parser.add_option(
+            "", "", "--commit-date", dest="commitdate", default = "",
+            help = "Set commit date for the project in %d/%m/%Y format"
+        )
+
+	parser.add_option(
+            "", "", "--importpath", dest="importpath", default = "",
+            help = "Set import path of the project"
+        )
+
+	options, args = parser.parse_args()
+
 	path = "."
-	commit_date = int(datetime.datetime.strptime('29/09/2015', '%d/%m/%Y').strftime("%s"))
-	noGodeps = Config().getSkippedDirectories()
-	importpath = "github.com/influxdb/influxdb"
+	if len(args):
+		path = args[0]
+
+	if not options.scanalldirs:
+		noGodeps = Config().getSkippedDirectories()
+	else:
+		noGodeps = []
+
+	if options.skipdirs:
+		for dir in options.skipdirs.split(','):
+			dir = dir.strip()
+			if dir == "":
+				continue
+			noGodeps.append(dir)
+
+	if options.commitdate == "":
+		commit_date = int(datetime.datetime.strptime('29/09/2015', '%d/%m/%Y').strftime("%s"))
+	else:
+		commit_date = int(datetime.datetime.strptime(options.commidate, '%d/%m/%Y').strftime("%s"))
+
+	if options.importpath == "":
+		importpath = "github.com/influxdb/influxdb"
+	else:
+		importpath = options.importpath
 
 	parser_config = ParserConfig()
 	parser_config.setSkipErrors()
@@ -42,13 +102,13 @@ if __name__ == "__main__":
 	parser_config.setParsePath(path)
 	parser_config.setImportPathPrefix(importpath)
 
-	da_obj = DependencyApproximator(parser_config, commit_date)
+	if not options.json:
+		sys.stderr.write("Missing --json option\n")
+		exit(1)
+
+	da_obj = DependencyApproximator(parser_config, commit_date, verbose=options.verbose)
 	da_obj.construct()
 	#print da_obj.getError()
 
-
-
-	#json_file["Deps"] = json_deps
-
-	#print json.dumps(json_file, indent=4, sort_keys=False)
-
+	deps = da_obj.getDependencies()
+	print deps2GodepsJson(deps, importpath)
