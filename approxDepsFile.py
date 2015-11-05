946d91957a80b3c73ad006633e16d6dae58d0ee9
Jan Chaloupka <jchaloup@redhat.com>
2015-11-05 12:12:47 +0100

    move code from approxDepsFile.py to new DependencyApproximator.py

--- approxDepsFile.py
+++ approxDepsFile.py
@@ -1,10 +1,8 @@
-from modules.Repos import Repos, getRepoCommits
+from modules.DependencyApproximator import DependencyApproximator
 from modules.Config import Config
-from modules.GoSymbolsExtractor import GoSymbolsExtractor
-from modules.ImportPathsDecomposer import ImportPathsDecomposer
-from modules.ImportPath import ImportPath
-import datetime
+
 import json
+import datetime
 
 from modules.ParserConfig import ParserConfig
 
@@ -15,34 +13,25 @@ from modules.ParserConfig import ParserConfig
 
 # optionally get a list of imported packages for given subset of project's packages
 
+def detectProjectSubpackages(prefix, imported_packages):
+	subpackages = []
+	prefix_len = len(prefix)
+	for ip in imported_packages:
+		if ip.startswith(prefix):
+			subpackage = ip[prefix_len:]
+			if subpackage == "":
+				subpackage = "."
+			else:
+				subpackage = subpackage[1:]
+			subpackages.append( subpackage )
+
+	return subpackages
 
 
 if __name__ == "__main__":
-	r_obj = Repos()
-	repos = r_obj.parseReposInfo()
-	local_repos = {}
-	upstream_repo = {}
-
-	for name in repos:
-		dir, repo = repos[name]
-
-		m_repo = str.replace(repo, 'https://', '')
-		m_repo = str.replace(m_repo, 'http://', '')
-		if m_repo.endswith('.git'):
-			m_repo = m_repo[:-4]
-		if m_repo.endswith('.hg'):
-			m_repo = m_repo[:-3]
-
-		local_repos[m_repo] = dir
-		upstream_repo[m_repo] = repo
-	#print local_repos
-
-	# 'golang-github-boltdb-bolt': ('/var/lib/gofed/packages/golang-github-boltdb-bolt/upstream//bolt', 'https://github.com/boltdb/bolt.git')
-	# print repos
 
 	path = "."
 	commit_date = int(datetime.datetime.strptime('29/09/2015', '%d/%m/%Y').strftime("%s"))
-	pull = True
 	noGodeps = Config().getSkippedDirectories()
 	importpath = "github.com/influxdb/influxdb"
 
@@ -51,75 +40,15 @@ if __name__ == "__main__":
 	parser_config.setNoGodeps(noGodeps)
 	parser_config.setImportsOnly()
 	parser_config.setParsePath(path)
+	parser_config.setImportPathPrefix(importpath)
 
-	gse_obj = GoSymbolsExtractor(parser_config)
-	if not gse_obj.extract():
-		fmt_obj.printError(gse_obj.getError())
-		exit(1)
+	da_obj = DependencyApproximator(parser_config)
+	da_obj.construct()
+	print da_obj.getError()
 
-	package_imports_occurence = gse_obj.getPackageImportsOccurences()
 
-	ip_used = gse_obj.getImportedPackages()
-	ipd = ImportPathsDecomposer(ip_used)
-	if not ipd.decompose():
-		fmt_obj.printError(ipd.getError())
-		exit(1)
 
-	warn = ipd.getWarning()
-	if warn != "":
-		fmt_obj.printWarning("Warning: %s" % warn)
+	#json_file["Deps"] = json_deps
 
-	classes = ipd.getClasses()
-	sorted_classes = sorted(classes.keys())
-
-	json_file = {}
-	json_file["ImportPath"] = importpath
-
-	json_deps = []
-	for element in sorted_classes:
-		if element == "Native":
-			continue
-
-		# class name starts with prefix => filter out
-		if importpath != "" and element.startswith(importpath):
-			continue
-
-		# convert each import path prefix to provider prefix
-		ip_obj = ImportPath(element)
-		if not ip_obj.parse():
-			print ip_obj.getError()
-			continue
-
-		provider_prefix = ip_obj.getProviderPrefix()
-		if provider_prefix not in local_repos:
-			print "%s:" % provider_prefix
-			continue
-
-		#print local_repos[provider_prefix]
-		path = local_repos[provider_prefix]
-		upstream = upstream_repo[provider_prefix]
-
-		commits = getRepoCommits(path, upstream, pull=pull)
-		last_commit_date = 1
-		last_commit = -1
-		for commit in commits:
-			if last_commit_date <= commits[commit]:
-				last_commit_date = commits[commit]
-				last_commit = commit
-			else:
-				break
-
-		str_date = datetime.datetime.fromtimestamp(int(last_commit_date)).strftime('%Y-%m-%d %H:%M:%S')
-
-		for ip in classes[element]:
-			line = {}
-			line["ImportPath"] = str(ip)
-			line["Comment"] = str_date
-			line["Rev"] = last_commit
-			json_deps.append(line)
-		#print "%s: %s" % (str(element), last_commit)
-
-	json_file["Deps"] = json_deps
-
-	print json.dumps(json_file, indent=4, sort_keys=False)
+	#print json.dumps(json_file, indent=4, sort_keys=False)
 
---
+++ modules/DependencyApproximator.py
@@ -0,0 +1,150 @@
+from Base import Base
+from GoSymbolsExtractor import GoSymbolsExtractor
+from ImportPathsDecomposer import ImportPathsDecomposer
+from ImportPath import ImportPath
+from modules.Repos import Repos, getRepoCommits
+import datetime
+
+class CommitHandler(Base):
+	"""
+	For a given commit and project get its closes commit
+	"""
+	def __init__(self, commit, project):
+		self.commit = commit
+		self.project = project
+
+class DependencyApproximator(Base):
+	"""
+	1) Provide a golang project and commit
+	2) For the project get a list of its dependencies and determinate
+	   the closest commit possible to the one provided (e.g. based on date)
+	3) Decompose imported packages into classes
+	4) For each class get a list of used packages (subset of project's packages)
+	5) Mark import path prefix as of each processed class to avoid cycles
+	6) For each new import path prefix found put it into a queue of projects to process (with a subset of packages to read)
+
+	Here, I have a list of direct dependencies. Now get a list of indirect dependencies
+	7) For each dependencies and a subset of its packages (in queue) construct a dependency graph of a project for given commit
+	8) From each node in the dependency graph get a list of imported packages.
+	9) For each list repeat step 2), 3), 4), 5) and 6)
+	10) Repeat steps 7)-10) until the queue is empty
+
+	There is a change to have two different commits for the same dependency (take the younger of them but report it to user)
+
+	"""
+	def __init__(self, parser_config):
+		Base.__init__(self)
+		self.err = []
+
+		self.parser_config = parser_config
+		self.import_path_prefix = self.parser_config.getImportPathPrefix()
+		self.pull = False
+
+		self.local_repos = {}
+		self.upstream_repo = {}
+		self.detected_commits = {}
+
+	def construct(self):
+		self.getRepos()
+		self.getDirectDependencies()
+
+	def getRepos(self):
+		r_obj = Repos()
+		repos = r_obj.parseReposInfo()
+		self.local_repos = {}
+		self.upstream_repo = {}
+
+		# 'golang-github-boltdb-bolt': ('/var/lib/gofed/packages/golang-github-boltdb-bolt/upstream//bolt', 'https://github.com/boltdb/bolt.git')
+		for name in repos:
+			dir, repo = repos[name]
+	
+			m_repo = str.replace(repo, 'https://', '')
+			m_repo = str.replace(m_repo, 'http://', '')
+			if m_repo.endswith('.git'):
+				m_repo = m_repo[:-4]
+			if m_repo.endswith('.hg'):
+				m_repo = m_repo[:-3]
+	
+			self.local_repos[m_repo] = dir
+			self.upstream_repo[m_repo] = repo
+
+	def detectProjectSubpackages(self, prefix, imported_packages):
+		subpackages = []
+		prefix_len = len(prefix)
+		for ip in imported_packages:
+			if ip.startswith(prefix):
+				subpackage = ip[prefix_len:]
+				if subpackage == "":
+					subpackage = "."
+				else:
+					subpackage = subpackage[1:]
+				subpackages.append( subpackage )
+	
+		return subpackages
+
+	def getDirectDependencies(self):
+
+		gse_obj = GoSymbolsExtractor(self.parser_config)
+		if not gse_obj.extract():
+			self.err.append(gse_obj.getError())
+			return False
+
+		package_imports_occurence = gse_obj.getPackageImportsOccurences()
+
+		ip_used = gse_obj.getImportedPackages()
+		ipd = ImportPathsDecomposer(ip_used)
+		if not ipd.decompose():
+			self.err.append(ipd.getError())
+			return False
+
+		self.warn = ipd.getWarning()
+
+		classes = ipd.getClasses()
+		sorted_classes = sorted(classes.keys())
+
+		for element in sorted_classes:
+			if element == "Native":
+				continue
+
+			# class name starts with prefix => filter out
+			if element.startswith(self.import_path_prefix):
+				continue
+
+			# convert each import path prefix to provider prefix
+			ip_obj = ImportPath(element)
+			if not ip_obj.parse():
+				self.err.append(ip_obj.getError())
+				continue
+
+			provider_prefix = ip_obj.getProviderPrefix()
+			if provider_prefix not in self.local_repos:
+				self.err.append("Repository for %s not found" % provider_prefix)
+				continue
+
+			#print self.local_repos[provider_prefix]
+			path = self.local_repos[provider_prefix]
+			upstream = self.upstream_repo[provider_prefix]
+
+			commits = getRepoCommits(path, upstream, pull=self.pull)
+			last_commit_date = 1
+			last_commit = -1
+			for commit in commits:
+				if last_commit_date <= commits[commit]:
+					last_commit_date = commits[commit]
+					last_commit = commit
+				else:
+					break
+
+			str_date = datetime.datetime.fromtimestamp(int(last_commit_date)).strftime('%Y-%m-%d %H:%M:%S')
+
+			print element + " (" + str(self.detectProjectSubpackages(element, classes[element])) + ")"
+			for ip in classes[element]:
+				info = {}
+				info["ImportPath"] = str(ip)
+				info["Comment"] = str_date
+				info["Rev"] = last_commit
+				self.detected_commits[ip] = info
+
+		print self.detected_commits
+
+		return True
