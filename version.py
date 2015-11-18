825170f7d23443d495018eb55a6c547e9fb6ed0e
Jan Chaloupka <jchaloup@redhat.com>
2015-11-18 12:56:21 +0100

    Update changelog and version

--- version.py
+++ version.py
@@ -1,5 +1,5 @@
-GOFED_VERSION = "0.0.9"
-GOFED_COMPILED = "10 September 2015"
+GOFED_VERSION = "0.0.10"
+GOFED_COMPILED = "18 November 2015"
 GOFED_AUTHOR = "Jan Chaloupka <jchaloup@redhat.com>"
 
 def printVersion():
