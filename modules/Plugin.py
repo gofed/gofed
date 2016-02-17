93ffaf9dce7d337c0d25e7888a90f6ce3e32a9ab
Jan Chaloupka <jchaloup@redhat.com>
2016-02-17 22:44:39 +0100

    Temporary hack to be able to run third_party python modules

--- modules/Plugin.py
+++ modules/Plugin.py
@@ -64,7 +64,7 @@ class PluginCmd:
 
 	def __init__(self, script):
 		self.script = script
-		self.interpret = "python"
+		self.interpret = "PYTHONPATH=/home/jchaloup/Projects/gofed/infra/third_party:/home/jchaloup/Projects/gofed/gofed/third_party python"
 		self.interactive = False
 
 	def getScript(self):
