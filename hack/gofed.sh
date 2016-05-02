7ae6562ecf9b5759b346d36033f5571a2a0577d4
Jan Chaloupka <jchaloup@redhat.com>
2016-05-02 21:44:36 +0200

    hack/gofed.sh: hack the PYTHONPATH to take gofed modules from WIP repos

--- hack/gofed.sh
+++ hack/gofed.sh
@@ -6,5 +6,6 @@ CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export GOFED_DEVEL=1
 # set PYTHONPATH
 export PYTHONPATH=${CUR_DIR}/../third_party:${CUR_DIR}/../..
+#export PYTHONPATH=/home/jchaloup/Projects/gofed/infra/third_party:/home/jchaloup/Projects/gofed:/home/jchaloup/Projects/gofed/gofed/third_party:/home/jchaloup/Projects/gofed
 
 ${CUR_DIR}/../gofed $@
