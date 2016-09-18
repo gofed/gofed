7fad438ffad5b0c71416283217c08b6b7a448aa1
Jan Chaloupka <jchaloup@redhat.com>
2016-09-18 17:38:32 +0200

    move cmd/gofed-docker to gofed-docker, detect all plugins automatically

--- hack/gofed-docker.sh
+++ hack/gofed-docker.sh
@@ -5,4 +5,4 @@ CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # set PYTHONPATH
 export PYTHONPATH=${CUR_DIR}/../third_party:${CUR_DIR}/../third_party/cmdsignature:${CUR_DIR}/../..
 
-${CUR_DIR}/../cmd/gofed-docker $@
+${CUR_DIR}/../gofed-docker $@
