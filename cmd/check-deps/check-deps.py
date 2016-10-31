f840fff9eecd8b094078fbff709bfd26aef19e0c
Jan Chaloupka <jchaloup@redhat.com>
2016-10-31 10:43:13 +0100

    extend gofed check-deps with --glidefile

--- cmd/check-deps/check-deps.py
+++ cmd/check-deps/check-deps.py
@@ -33,6 +33,8 @@ if __name__ == "__main__":
 
 	if options.godeps != "":
 		snapshot = Snapshot().readGodepsFile(options.godeps)
+	elif options.glidefile != "":
+		snapshot = Snapshot().readGlideLockFile(options.glidefile)
 	elif options.glogfile != "":
 		snapshot = Snapshot().readGLOGFILE(options.glogfile)
 
